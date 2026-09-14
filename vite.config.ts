e2655d13de927fd785e78debc15d1566af52505c
ARROW <rishabbhkumar0002@gmail.com>
2026-09-14 23:18:04 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+  return { 
+    base: '/School-website/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
